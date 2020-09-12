aad06d32f19b6d79b24376731c6e1c02de17e015
Patrick <patrick.lopdrup@gmail.com>
2020-09-12 16:21:15 +0200

    Made debug mode and start of dividend worksheet

--- load_stocks.py
+++ load_stocks.py
@@ -4,12 +4,12 @@ import stock
 import payout_freq
 import dividend
 import re
+from pprint import pprint
+import my_debug
 
-
-nordnet = nordnet.Nordnet()
-saxo = saxo.Saxo()
-
-stocks = []
+if not my_debug.is_debug:
+    nordnet = nordnet.Nordnet()
+    saxo = saxo.Saxo()
 
 
 def fix_names(names):
@@ -47,13 +47,16 @@ def load_payout_dates(stock_amount, currencies, symbols):
     return payout_dates
 
 
-def get_stocks():
-    get_nordnet_stocks()
-    get_saxo_stocks()
+def get_all_stocks():
+    ''' Returns a list of all stocks '''
+    nordnet_list = get_nordnet_stocks()
+    saxo_list = get_saxo_stocks()
+    return nordnet_list + saxo_list
 
 
 def get_nordnet_stocks():
     # Nordnet
+    stocks = []
     stock_amount = nordnet.get_stock_amount()
     temp_names = nordnet.get_names()
     names = fix_names(temp_names)
@@ -73,8 +76,6 @@ def get_nordnet_stocks():
     frequencies = load_frequencies(stock_amount, currencies, symbols)
     payout_dates = load_payout_dates(stock_amount, currencies, symbols)
 
-    print(payout_dates)
-
     for i in range(stock_amount):
         stocks.append(stock.Stock(names[i], symbols[i], shares[i], costs[i], market_values[i], dividends[i],
                                   frequencies[i], payout_dates[i], sectors[i], currencies[i], conversion_rates[i], ids[i], broker))
@@ -83,6 +84,7 @@ def get_nordnet_stocks():
 
 def get_saxo_stocks():
     # Saxo
+    stocks = []
     stock_amount = saxo.get_stock_amount()
     names = saxo.get_names()
     symbols = saxo.get_symbols()
@@ -109,4 +111,8 @@ def get_saxo_stocks():
 
 
 if __name__ == '__main__':
-    print(get_saxo_stocks())
+    hej = stock.Stock.test_stock()
+    print(f'cost: {hej.cost}')
+    print(f'cost in dkk: {hej.cost_in_dkk()}')
+    hej.print_all()
+    
--- sheet_dividend.py
+++ sheet_dividend.py
@@ -1,2 +1,15 @@
 import payout_freq
-import 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('Aktier').get_worksheet(2)
+
+
+sheet.update_cell(15, 2, "hej")
+
